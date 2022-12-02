d79d379dd0bf4395e86e9a7f646272f35e7f4143
Temhja <husseinalsalih2010@gmail.com>
2022-12-02 13:54:30 +0300

    collapse

--- public/core.js
+++ public/core.js
@@ -1,9 +1,9 @@
-var coll = document.getElementsByClassName("collapse");
+var coll = document.getElementsByClassName("collapse-toggle");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("true");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
